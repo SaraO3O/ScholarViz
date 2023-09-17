0de62e4a1512f4b45767b8df01f18814bcf6ca72
SaraO3O <sarahferreirah1@gmail.com>
2023-09-16 21:38:49 -0300

    Botões funcionando

--- ScholarViz/static/index.js
+++ ScholarViz/static/index.js
@@ -2,29 +2,37 @@ const container = document.getElementById('sigma-container');
 const connectionsInfo = document.getElementById('connections-info');
 const searchInput = document.getElementById("search-input");
 const searchSuggestions = document.getElementById("suggestions");
+let graph; 
 
-// function loadRedeCitacaoGrafo(); {
-//   fetch(STATIC_ROOT+'/graphs/grafo1.json')
-//     .then(response => response.json())
-//     .then(serializedGraph => {
-//       // Lógica para desenhar o grafo com Sigma.js usando serializedGraph
-//       // Certifique-se de criar um novo gráfico e renderizá-lo aqui
-//     });
-// }
+function loadRedeCitacaoGrafo() {
+  const graphPath = '/graphs/gCitacoes.json';
+  loadAndRenderGraph(graphPath);
+}
 
-// loadRedeCitacaoGrafo();
+function loadRedeFiliacaoGrafo() {
+  const graphPath = '/graphs/gFiliacao.json';
+  loadAndRenderGraph(graphPath);
+}
 
+function loadRedeFiliacaoDegree() {
+  const graphPath = '/graphs/gFiliacaoDegree.json';
+  loadAndRenderGraph(graphPath);
+}
 
-const response = fetch(STATIC_ROOT+'/graphs/gCitacoes.json').then(response => {
-  response.json().then(serializedGraph => {
-    const graph = new graphology.Graph();
+function loadRedeFiliacaoBetweenness() {
+  const graphPath = '/graphs/gFiliacaoBetweenness.json';
+  loadAndRenderGraph(graphPath);
+}
+
+function loadAndRenderGraph(graphPath) {
+  fetch(STATIC_ROOT + graphPath).then(response => response.json()).then(serializedGraph => {
+    graph = new graphology.Graph();
     console.log("Loading json into graph...")
     graph.import(serializedGraph);
     container.innerHTML = "";
 
     const renderer = new Sigma(graph, container);
       
-
     // Redutores
     renderer.setSetting("nodeReducer", (node, data) => {
       const nodeDegree = graph.degree(node);
@@ -32,48 +40,6 @@ const response = fetch(STATIC_ROOT+'/graphs/gCitacoes.json').then(response => {
       data.label = `${label} (${nodeDegree} conexões)`;
       return data;
     });
-
-    renderer.setSetting("edgeReducer", (edge, data) => {
-      return data;
-    });
-
-
-  const searchInput = document.getElementById('search-input');
-  const searchSuggestions = document.getElementById('suggestions');
-
-  searchInput.addEventListener('input', () => {
-    const query = searchInput.value;
-    setSearchQuery(query);
-    showSearchSuggestions(query);
-  });
-
-  function setSearchQuery(query) {
-    renderer.setSetting("nodeReducer", (node, data) => {
-      const label = graph.getNodeAttribute(node, 'label');
-      if (label.toLowerCase().includes(query.toLowerCase())) {
-        data.hidden = false;
-      } else {
-        data.hidden = true;
-      }
-      return data;
-    });
-    renderer.refresh();
-  }
-
-  function showSearchSuggestions(query) {
-    const lcQuery = query.toLowerCase();
-    const suggestions = graph
-      .nodes()
-      .map((n) => graph.getNodeAttribute(n, 'label'))
-      .filter((label) => label.toLowerCase().includes(lcQuery));
-
-    searchSuggestions.innerHTML = '';
-    suggestions.forEach((label) => {
-      const suggestionOption = document.createElement('option');
-      suggestionOption.value = label;
-      searchSuggestions.appendChild(suggestionOption);
-    });
-  }
 
     // Exibição do vizinho do nó destacado
     let hoveredNode = null;
@@ -109,23 +75,72 @@ const response = fetch(STATIC_ROOT+'/graphs/gCitacoes.json').then(response => {
       }
       return data;
     });
-
-    // Atualizar as informações de conexões na div connections-info
-    function updateConnectionsInfo(node) {
-      connectionsInfo.innerHTML = '';
-      if (node) {
-        connectionsInfo.style.display = 'block'; // Exibe o connections-info
-        const nodeDegree = graph.degree(node);
-        const label = graph.getNodeAttribute(node, 'label');
-        const nodeInfo = document.createElement('p');
-        nodeInfo.innerText = `${label}: ${nodeDegree} conexões`;
-        connectionsInfo.appendChild(nodeInfo);
-      }
-    }
-
   });
+}
+
+//Carregamento inicial
+loadRedeCitacaoGrafo();
+
+searchInput.addEventListener('input', () => {
+  const query = searchInput.value;
+  setSearchQuery(query);
+  showSearchSuggestions(query);
 });
+
+function setSearchQuery(query) {
+  renderer.setSetting("nodeReducer", (node, data) => {
+    const label = graph.getNodeAttribute(node, 'label');
+    if (label.toLowerCase().includes(query.toLowerCase())) {
+      data.hidden = false;
+    } else {
+      data.hidden = true;
+    }
+    return data;
+  });
+  renderer.refresh();
+}
+
+function showSearchSuggestions(query) {
+  const lcQuery = query.toLowerCase();
+  const suggestions = graph
+    .nodes()
+    .map((n) => graph.getNodeAttribute(n, 'label'))
+    .filter((label) => label.toLowerCase().includes(lcQuery));
+
+  searchSuggestions.innerHTML = '';
+  suggestions.forEach((label) => {
+    const suggestionOption = document.createElement('option');
+    suggestionOption.value = label;
+    searchSuggestions.appendChild(suggestionOption);
+  });
+}
+
+  // Atualizar as informações de conexões na div connections-info
+  function updateConnectionsInfo(node) {
+    connectionsInfo.innerHTML = '';
+    if (node) {
+      connectionsInfo.style.display = 'block'; // Exibe o connections-info
+      const nodeDegree = graph.degree(node);
+      const label = graph.getNodeAttribute(node, 'label');
+      const nodeInfo = document.createElement('p');
+      nodeInfo.innerText = `${label}: ${nodeDegree} conexões`;
+      connectionsInfo.appendChild(nodeInfo);
+    }
+  }
+
 
 document.getElementById("redeCitacao").addEventListener("click", function () {
   loadRedeCitacaoGrafo();
+});
+
+document.getElementById("redeFiliacao").addEventListener("click", function () {
+  loadRedeFiliacaoGrafo();
+});
+
+document.getElementById("redeFiliacaoBetweenness").addEventListener("click", function () {
+  loadRedeFiliacaoBetweenness();
+});
+
+document.getElementById("redeFiliacaoDegree").addEventListener("click", function () {
+  loadRedeFiliacaoDegree();
 });
